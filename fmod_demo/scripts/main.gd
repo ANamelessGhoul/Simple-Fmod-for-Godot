86ace4139eacc1657d25fb51eb71df683ba3dcf8
Tecelli Eriz Akintug <tecelli.e.a@gmail.com>
2026-08-14 09:58:56 +0200

    Updated example project to support lower godot versions and controller input

--- fmod_demo/scripts/main.gd
+++ fmod_demo/scripts/main.gd
@@ -1,9 +1,11 @@
 extends Node
 
-onready var volume_slider = $"%VolumeSlider"
+onready var volume_slider = $CanvasLayer/VBoxContainer/VolumeSlider
+onready var play_button = $CanvasLayer/VBoxContainer/PlayButton
 
 func _ready():
 	volume_slider.value = FmodInterface.get_bus_volume("bus:/")
+	play_button.grab_focus()
 
 func _on_VolumeSlider_value_changed(value):
 	FmodInterface.set_bus_volume("bus:/", value)
